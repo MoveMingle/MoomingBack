5b5483683ae3ec54bf4c3b9fc14c534606f9aaea
yegenie <geniest92@gmail.com>
2024-02-07 21:25:27 +0900

    first commit

---
+++ src/main/java/com/example/mooming/MoomingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mooming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoomingApplication.class, args);
+	}
+
+}
